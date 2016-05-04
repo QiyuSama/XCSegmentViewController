532a1e58b1410767223f2c9e207a5fa212655316
xiangchao <455877644@qq.com>
2016-05-04 10:26:39 +0800

    Initial Commit

---
+++ XCSelectionViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCSelectionViewController
+//
+//  Created by xiangchao on 16/5/4.
+//  Copyright © 2016年 STV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
